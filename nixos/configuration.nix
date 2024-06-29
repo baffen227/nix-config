ee90c3cf4bbc502eddb8e741e0526ee8c72890f6
Harry Chen <baffen227@gmail.com>
2024-06-29 22:31:27 +0800

    Update configuration.nix to comment out authorizedKeys

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -143,9 +143,9 @@
       description = "baffen227"
 
       # https://nixos.wiki/wiki/SSH_public_key_authentication
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
+      #openssh.authorizedKeys.keys = [
+      #  # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      #];
 
       # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = [ "networkmanager" "wheel" ];
