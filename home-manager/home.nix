84eb6709fc243474035c64c92ccc45af85f8a7d3
Harry Chen <baffen227@gmail.com>
2024-07-13 23:45:39 +0800

    Update settings of alacritty

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -119,14 +119,19 @@
   programs.git.enable = true;
 
   # Enable and setup alacritty
+  # https://alacritty.org/config-alacritty.html
   programs.alacritty = {
     enable = true;
     settings = {
       env.TERM = "xterm-256color";
       font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
+        normal = {
+	  family = "Source Code Pro";
+	  style = "Regular";
+	};
+	size = 15;
       };
+      colors.draw_bold_text_with_bright_colors = true;
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
     };
